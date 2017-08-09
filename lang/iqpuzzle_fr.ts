69f5c94bd3a6be945afffbec41b0247888ed6505
Ferry Jérémie <ferryjeremie@free.fr>
2017-08-09 18:38:11 +0200

    french translation

---
+++ lang/iqpuzzle_fr.ts
@@ -0,0 +1,389 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>CBoard</name>
+    <message>
+        <location filename="../CBoard.cpp" line="54"/>
+        <location filename="../CBoard.cpp" line="86"/>
+        <location filename="../CBoard.cpp" line="171"/>
+        <location filename="../CBoard.cpp" line="191"/>
+        <location filename="../CBoard.cpp" line="217"/>
+        <location filename="../CBoard.cpp" line="246"/>
+        <location filename="../CBoard.cpp" line="254"/>
+        <location filename="../CBoard.cpp" line="359"/>
+        <source>Warning</source>
+        <translation>Avertissement</translation>
+    </message>
+    <message>
+        <location filename="../CBoard.cpp" line="55"/>
+        <source>Board grid size not valid.
+Reduced grid to default.</source>
+        <translation>Grille de plateau invalide. Taille de grille réduite à la valeur par défaut.</translation>
+    </message>
+    <message>
+        <location filename="../CBoard.cpp" line="87"/>
+        <source>Board polygon not valid.</source>
+        <translation>Plateau polygonal invalide.</translation>
+    </message>
+    <message>
+        <location filename="../CBoard.cpp" line="172"/>
+        <location filename="../CBoard.cpp" line="218"/>
+        <source>Polygon not valid:</source>
+        <translation>Polygone invalide:</translation>
+    </message>
+    <message>
+        <location filename="../CBoard.cpp" line="192"/>
+        <source>Could not find valid blocks.</source>
+        <translation>Impossible de trouver un bloc valable.</translation>
+    </message>
+    <message>
+        <location filename="../CBoard.cpp" line="247"/>
+        <source>No color defined - using fallback:</source>
+        <translation>Aucune couleur définie - Utilisation du mode fallback:</translation>
+    </message>
+    <message>
+        <location filename="../CBoard.cpp" line="255"/>
+        <source>Invalid color defined - using fallback:</source>
+        <translation>La couleur définie est invalide - Utilisation du mode fallback:</translation>
+    </message>
+    <message>
+        <location filename="../CBoard.cpp" line="360"/>
+        <source>Invalid start position - using fallback:</source>
+        <translation>Position de départ invalide - Utilisation du mode fallback:</translation>
+    </message>
+    <message>
+        <location filename="../CBoard.cpp" line="139"/>
+        <source>Hint</source>
+        <translation>Indication</translation>
+    </message>
+    <message>
+        <location filename="../CBoard.cpp" line="140"/>
+        <source>Not all pieces are needed for a solution!</source>
+        <translation>Toutes les pièces ne sont pas nécessaires pour une solution!</translation>
+    </message>
+</context>
+<context>
+    <name>CBoardDialog</name>
+    <message>
+        <location filename="../CBoardDialog.cpp" line="42"/>
+        <location filename="../CBoardDialog.cpp" line="72"/>
+        <source>Solutions</source>
+        <translation>Solutions</translation>
+    </message>
+    <message>
+        <location filename="../CBoardDialog.cpp" line="43"/>
+        <source>Preview</source>
+        <translation>Aperçu</translation>
+    </message>
+    <message>
+        <location filename="../CBoardDialog.cpp" line="70"/>
+        <source>Unknown</source>
+        <translation>Inconnu</translation>
+    </message>
+    <message>
+        <location filename="../CBoardDialog.cpp" line="79"/>
+        <source>No preview available</source>
+        <translation>Pas d'aperçu disponible</translation>
+    </message>
+</context>
+<context>
+    <name>CHighscore</name>
+    <message>
+        <location filename="../CHighscore.cpp" line="58"/>
+        <location filename="../CHighscore.cpp" line="157"/>
+        <source>Highscore</source>
+        <translation>Meilleurs scores</translation>
+    </message>
+    <message>
+        <location filename="../CHighscore.cpp" line="66"/>
+        <source>Position</source>
+        <translation>Rang</translation>
+    </message>
+    <message>
+        <location filename="../CHighscore.cpp" line="68"/>
+        <source>Name</source>
+        <translation>Nom</translation>
+    </message>
+    <message>
+        <location filename="../CHighscore.cpp" line="70"/>
+        <source>Time</source>
+        <translation>Temps</translation>
+    </message>
+    <message>
+        <location filename="../CHighscore.cpp" line="72"/>
+        <source>Moves</source>
+        <translation>Coups</translation>
+    </message>
+    <message>
+        <location filename="../CHighscore.cpp" line="158"/>
+        <source>Please insert your name for a new highscore:</source>
+        <translation>Indiquez votre nom pour les meilleurs scores:</translation>
+    </message>
+</context>
+<context>
+    <name>CIQPuzzle</name>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="41"/>
+        <source>&amp;Game</source>
+        <translation>&amp;Jeu</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="57"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Aide</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="64"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="78"/>
+        <source>Choose &amp;new game</source>
+        <translation>&amp;Nouvelle partie</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="83"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Quitter</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="88"/>
+        <source>&amp;About</source>
+        <translation>&amp;A propos</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="93"/>
+        <source>Zoom &amp;In</source>
+        <translation>Agrand&amp;ir</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="98"/>
+        <source>Zoom &amp;Out</source>
+        <translation>&amp;Rétrécir</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="106"/>
+        <source>&amp;Restart game</source>
+        <translation>&amp;Redémarrer la partie</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="114"/>
+        <source>&amp;Save game</source>
+        <translation>&amp;Sauvegarder la partie</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="117"/>
+        <location filename="../CIQPuzzle.cpp" line="414"/>
+        <source>Save game</source>
+        <translation>Sauvegarder la partie</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="122"/>
+        <source>&amp;Load game</source>
+        <translation>&amp;Ouvrir une partie</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="125"/>
+        <location filename="../CIQPuzzle.cpp" line="391"/>
+        <source>Load game</source>
+        <translation>Ouvrir une partie</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="136"/>
+        <source>Pa&amp;use</source>
+        <translation>Pa&amp;use</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="141"/>
+        <source>&amp;Highscore</source>
+        <translation>&amp;Meilleurs scores</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="146"/>
+        <source>&amp;Configure iQPuzzle...</source>
+        <translation>&amp;Configurer iQPuzzle...</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="151"/>
+        <source>Report &amp;bug</source>
+        <translation>Signaler un &amp;bug</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.ui" line="156"/>
+        <source>R&amp;andom game</source>
+        <translation>P&amp;artie aléatoire</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="293"/>
+        <source>Load board</source>
+        <translation>Charger une partie</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="240"/>
+        <location filename="../CIQPuzzle.cpp" line="251"/>
+        <source>File not found</source>
+        <translation>Fichier non trouvé</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="241"/>
+        <location filename="../CIQPuzzle.cpp" line="252"/>
+        <source>The chosen file does not exist.</source>
+        <translation>Le fichier sélectionné n'existe pas.</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="278"/>
+        <source>Unknown</source>
+        <translation>Inconnu</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="405"/>
+        <source>Invalid saved puzzle.</source>
+        <translation>Puzzle enregistré invalide.</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="83"/>
+        <location filename="../CIQPuzzle.cpp" line="595"/>
+        <source>Game paused</source>
+        <translation>Partie en pause</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="87"/>
+        <location filename="../CIQPuzzle.cpp" line="260"/>
+        <location filename="../CIQPuzzle.cpp" line="266"/>
+        <location filename="../CIQPuzzle.cpp" line="471"/>
+        <location filename="../CIQPuzzle.cpp" line="484"/>
+        <source>Time</source>
+        <translation>Temps</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="88"/>
+        <location filename="../CIQPuzzle.cpp" line="257"/>
+        <location filename="../CIQPuzzle.cpp" line="264"/>
+        <location filename="../CIQPuzzle.cpp" line="462"/>
+        <location filename="../CIQPuzzle.cpp" line="483"/>
+        <location filename="../CIQPuzzle.cpp" line="599"/>
+        <source>Moves</source>
+        <translation>Coups</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="295"/>
+        <source>Board files</source>
+        <translation>Plateaux</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="283"/>
+        <source>Solutions</source>
+        <translation>Solutions</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="482"/>
+        <source>Puzzle solved!</source>
+        <translation>Puzzle résolu!</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="553"/>
+        <source>About</source>
+        <translation>A propos</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="569"/>
+        <source>License</source>
+        <translation>Licence</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="573"/>
+        <source>This application uses icons from &lt;a href=&quot;http://tango.freedesktop.org&quot;&gt;Tango project&lt;/a&gt;.</source>
+        <translation>Ce logiciel utilise des icônes de &lt;a href=&quot;http://tango.freedesktop.org&quot;&gt;Tango Projekt&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <location filename="../CIQPuzzle.cpp" line="576"/>
+        <source>Translations</source>
+        <translation>Traductions</translation>
+    </message>
+</context>
+<context>
+    <name>CSettings</name>
+    <message>
+        <location filename="../CSettings.cpp" line="56"/>
+        <location filename="../CSettings.cpp" line="202"/>
+        <source>Left</source>
+        <translation>Gauche</translation>
+    </message>
+    <message>
+        <location filename="../CSettings.cpp" line="56"/>
+        <location filename="../CSettings.cpp" line="202"/>
+        <source>Middle</source>
+        <translation>Milieu</translation>
+    </message>
+    <message>
+        <location filename="../CSettings.cpp" line="56"/>
+        <location filename="../CSettings.cpp" line="202"/>
+        <source>Right</source>
+        <translation>Droite</translation>
+    </message>
+    <message>
+        <location filename="../CSettings.cpp" line="59"/>
+        <location filename="../CSettings.cpp" line="206"/>
+        <source>First X</source>
+        <translation>First X</translation>
+    </message>
+    <message>
+        <location filename="../CSettings.cpp" line="59"/>
+        <location filename="../CSettings.cpp" line="206"/>
+        <source>Second X</source>
+        <translation>Second X</translation>
+    </message>
+    <message>
+        <location filename="../CSettings.cpp" line="60"/>
+        <location filename="../CSettings.cpp" line="207"/>
+        <source>Horizontal wheel</source>
+        <translation>Molette horizontale</translation>
+    </message>
+    <message>
+        <location filename="../CSettings.cpp" line="60"/>
+        <location filename="../CSettings.cpp" line="207"/>
+        <source>Vertical wheel</source>
+        <translation>Molette verticale</translation>
+    </message>
+    <message>
+        <location filename="../CSettings.cpp" line="113"/>
+        <source>Please change your settings. Same mouse button is used for several actions.</source>
+        <translation>Merci de changer les paramètres. Le même bouton est utilisé pour plusieurs actions.</translation>
+    </message>
+</context>
+<context>
+    <name>CSettingsDialog</name>
+    <message>
+        <location filename="../CSettings.ui" line="14"/>
+        <source>Preferences</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../CSettings.ui" line="32"/>
+        <source>Mouse controls</source>
+        <translation>Contrôle avec la souris</translation>
+    </message>
+    <message>
+        <location filename="../CSettings.ui" line="39"/>
+        <source>Move block</source>
+        <translation>Bouger un bloc</translation>
+    </message>
+    <message>
+        <location filename="../CSettings.ui" line="49"/>
+        <source>Rotate block</source>
+        <translation>Tourner un bloc</translation>
+    </message>
+    <message>
+        <location filename="../CSettings.ui" line="59"/>
+        <source>Flip block</source>
+        <translation>Inverser un bloc</translation>
+    </message>
+    <message>
+        <location filename="../CSettings.ui" line="76"/>
+        <source>GUI language</source>
+        <translation>Langue de l'interface</translation>
+    </message>
+</context>
+</TS>
