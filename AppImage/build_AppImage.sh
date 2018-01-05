5682b80c6041be4fe0ea645773d84c80752d03c0
ElTh0r0 <elthoro@gmx.de>
2018-01-05 20:08:10 +0100

    macOS and AppImage scripts

---
+++ AppImage/build_AppImage.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+echo "Building..."
+qmake CONFIG+=release PREFIX=/usr
+make -j$(nproc)
+make INSTALL_ROOT=appdir -j$(nproc) install
+find appdir
+
+echo "Downloading linuxdeployqt..."
+wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+unset QTDIR ; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+export VERSION=$(git rev-parse --short HEAD)  # linuxdeployqt uses this for naming the file
+
+echo "Creating AppImage..."
+cp ./res/images/iqpuzzle_64x64.png ./appdir/iqpuzzle.png
+./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -appimage -bundle-non-qt-libs
+find ./appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
+
+echo "Uploading to:"
+curl --upload-file iQPuzzle-$VERSION-x86_64.AppImage https://transfer.sh/iqPuzzle-$VERSION-x86_64.AppImage
+echo ""
+
+echo "Done!"
+
+exit 0
---
+++ macOS/build_macOS.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+APP='iQPuzzle'
+
+# Hold on to current directory
+project_dir=$(pwd)
+
+# Output macOS version
+sw_vers
+
+# Update platform (enable if needed)
+#echo "Updating platform..."
+#brew update
+
+# Install p7zip for packaging
+brew install p7zip
+
+# Install Qt
+echo "Installing Qt..."
+brew install qt
+
+# Add Qt binaries to path
+PATH=/usr/local/opt/qt/bin/:${PATH}
+
+# Build app
+echo "Building..."
+cd ${project_dir}
+mkdir build
+cd build
+qmake ../iqpuzzle.pro CONFIG+=release
+make
+
+# Build and run tests here
+
+# Package
+echo "Packaging..."
+
+# Remove build directories that should not be deployed
+rm -rf .moc
+rm -rf .obj
+rm -rf .qrc
+
+echo "Creating dmg archive..."
+macdeployqt "${APP}.app" -dmg
+mv "${APP}.dmg" "${APP}_${REV_NAME}.dmg"
+
+# Copy other project files
+cp "${project_dir}/README.md" "${project_dir}/build/README.md"
+cp "${project_dir}/COPYING" "${project_dir}/build/COPYING"
+
+echo "Packaging zip archive..."
+7z a ${APP}_${REV_NAME}_macOS.zip "${APP}_${REV_NAME}.dmg" "README.md" "COPYING"
+
+echo "Uploading to:"
+curl --upload-file ${APP}_${REV_NAME}_macOS.zip https://transfer.sh/${APP}_${REV_NAME}_macOS.zip
+echo ""
+
+echo "Done!"
+
+exit 0
