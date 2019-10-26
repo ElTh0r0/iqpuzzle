7233e4f1edf951f80002f4ce9fea70924ed125bd
hyuna1127 <52125096+hyuna1127@users.noreply.github.com>
2019-10-27 02:25:35 +0900

    Add files via upload

---
+++ lang/iqpuzzle_ko.ts
@@ -0,0 +1,485 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR">
+<context>
+    <name>Board</name>
+    <message>
+        <location filename="../board.cpp" line="59"/>
+        <location filename="../board.cpp" line="75"/>
+        <location filename="../board.cpp" line="179"/>
+        <location filename="../board.cpp" line="201"/>
+        <location filename="../board.cpp" line="224"/>
+        <location filename="../board.cpp" line="253"/>
+        <location filename="../board.cpp" line="261"/>
+        <location filename="../board.cpp" line="367"/>
+        <source>Warning</source>
+        <translation type="unfinished">경고</translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="59"/>
+        <source>Board grid size not valid.
+Reduced grid to default.</source>
+        <translation type="unfinished">현재 보드판의 크기는 사용할 수 없습니다.
+기본 값으로 줄여주세요.</translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="75"/>
+        <source>Board polygon not valid.</source>
+        <translation type="unfinished">사용할 수 없는 크기의 다각형입니다.</translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="150"/>
+        <source>Hint</source>
+        <translation type="unfinished">힌트</translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="150"/>
+        <source>Not all pieces are needed for a solution!</source>
+        <translation type="unfinished">정답에 모든 조각들이 필요한 것은 아닙니다!</translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="180"/>
+        <location filename="../board.cpp" line="225"/>
+        <source>Polygon not valid:</source>
+        <translation type="unfinished">사용할 수 없는 다각형:</translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="201"/>
+        <source>Could not find valid blocks.</source>
+        <translation type="unfinished">사용할 수 있는 블록이 없습니다.</translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="254"/>
+        <source>No color defined - using fallback:</source>
+        <translation type="unfinished">정의된 색상 없음 - fallback 사용:</translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="262"/>
+        <source>Invalid color defined - using fallback:</source>
+        <translation type="unfinished">색상 정의 불가 - fallback 사용:</translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="368"/>
+        <source>Invalid start position - using fallback:</source>
+        <translation type="unfinished">허용되지 않은 시작 위치 - fallback 사용:</translation>
+    </message>
+</context>
+<context>
+    <name>BoardDialog</name>
+    <message>
+        <location filename="../boarddialog.cpp" line="42"/>
+        <location filename="../boarddialog.cpp" line="72"/>
+        <source>Solutions</source>
+        <translation type="unfinished">해결</translation>
+    </message>
+    <message>
+        <location filename="../boarddialog.cpp" line="43"/>
+        <source>Preview</source>
+        <translation type="unfinished">미리보기</translation>
+    </message>
+    <message>
+        <location filename="../boarddialog.cpp" line="70"/>
+        <source>Unknown</source>
+        <translation type="unfinished">알수없음</translation>
+    </message>
+    <message>
+        <location filename="../boarddialog.cpp" line="79"/>
+        <source>No preview available</source>
+        <translation type="unfinished">미리보기 불가능</translation>
+    </message>
+</context>
+<context>
+    <name>Highscore</name>
+    <message>
+        <location filename="../highscore.cpp" line="59"/>
+        <location filename="../highscore.cpp" line="157"/>
+        <source>Highscore</source>
+        <translation type="unfinished">최고점</translation>
+    </message>
+    <message>
+        <location filename="../highscore.cpp" line="67"/>
+        <source>Position</source>
+        <translation type="unfinished">위치</translation>
+    </message>
+    <message>
+        <location filename="../highscore.cpp" line="69"/>
+        <source>Name</source>
+        <translation type="unfinished">이름</translation>
+    </message>
+    <message>
+        <location filename="../highscore.cpp" line="71"/>
+        <source>Time</source>
+        <translation type="unfinished">시간</translation>
+    </message>
+    <message>
+        <location filename="../highscore.cpp" line="73"/>
+        <source>Moves</source>
+        <translation type="unfinished">이동횟수</translation>
+    </message>
+    <message>
+        <location filename="../highscore.cpp" line="158"/>
+        <source>Please insert your name for a new highscore:</source>
+        <translation type="unfinished">신기록을 위해 당신의 이름을 기입하세요:</translation>
+    </message>
+</context>
+<context>
+    <name>IQPuzzle</name>
+    <message>
+        <location filename="../iqpuzzle.ui" line="41"/>
+        <source>&amp;Game</source>
+        <translation type="unfinished">&amp;게임</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="45"/>
+        <source>R&amp;andom game</source>
+        <translation type="unfinished">랜&amp;덤 게임</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="53"/>
+        <source>&amp;All available</source>
+        <translation type="unfinished">&amp;모두 허용</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="63"/>
+        <source>All &amp;unsolved</source>
+        <translation type="unfinished">모두 &amp;미해결</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="89"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished">&amp;도움</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="96"/>
+        <source>Setti&amp;ngs</source>
+        <translation type="unfinished">셋&amp;팅</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="115"/>
+        <source>Choose &amp;new game</source>
+        <translation type="unfinished">선택 &amp;새로운 게임</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="125"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished">&amp;종료</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="135"/>
+        <source>&amp;About</source>
+        <translation type="unfinished">&amp;상세정보</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="145"/>
+        <source>Zoom &amp;In</source>
+        <translation type="unfinished">&amp;확대</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="155"/>
+        <source>Zoom &amp;Out</source>
+        <translation type="unfinished">&amp;축소</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="168"/>
+        <source>&amp;Restart game</source>
+        <translation type="unfinished">&amp;게임 재시작</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="181"/>
+        <source>&amp;Save game</source>
+        <translation type="unfinished">&amp;게임 저장</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="184"/>
+        <location filename="../iqpuzzle.cpp" line="501"/>
+        <source>Save game</source>
+        <translation type="unfinished">게임 저장</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="194"/>
+        <source>&amp;Load game</source>
+        <translation type="unfinished">&amp;게임 불러오기</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="197"/>
+        <location filename="../iqpuzzle.cpp" line="474"/>
+        <source>Load game</source>
+        <translation type="unfinished">게임 불러오기</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="213"/>
+        <source>Pa&amp;use</source>
+        <translation type="unfinished">중&amp;지</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="218"/>
+        <source>&amp;Highscore</source>
+        <translation type="unfinished">&amp;기록</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="228"/>
+        <source>&amp;Configure iQPuzzle...</source>
+        <translation type="unfinished">&amp;iQPuzzle에 관하여...</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="233"/>
+        <source>Report &amp;bug</source>
+        <translation type="unfinished">버그 &amp;리포팅</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="238"/>
+        <location filename="../iqpuzzle.ui" line="263"/>
+        <source>&amp;Easy</source>
+        <translation type="unfinished">&amp;쉬움</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="243"/>
+        <location filename="../iqpuzzle.ui" line="268"/>
+        <source>&amp;Medium</source>
+        <translation type="unfinished">&amp;중간</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="248"/>
+        <location filename="../iqpuzzle.ui" line="273"/>
+        <source>&amp;Hard</source>
+        <translation type="unfinished">&amp;어려움</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="253"/>
+        <location filename="../iqpuzzle.ui" line="258"/>
+        <source>&amp;All</source>
+        <translation type="unfinished">&amp;전체</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="278"/>
+        <source>Sta&amp;tistics</source>
+        <translation type="unfinished">통&amp;계</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="84"/>
+        <location filename="../iqpuzzle.cpp" line="762"/>
+        <source>Game paused</source>
+        <translation type="unfinished">게임 중지</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="88"/>
+        <location filename="../iqpuzzle.cpp" line="280"/>
+        <location filename="../iqpuzzle.cpp" line="286"/>
+        <location filename="../iqpuzzle.cpp" line="568"/>
+        <location filename="../iqpuzzle.cpp" line="581"/>
+        <source>Time</source>
+        <translation type="unfinished">시간</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="89"/>
+        <location filename="../iqpuzzle.cpp" line="277"/>
+        <location filename="../iqpuzzle.cpp" line="284"/>
+        <location filename="../iqpuzzle.cpp" line="560"/>
+        <location filename="../iqpuzzle.cpp" line="580"/>
+        <location filename="../iqpuzzle.cpp" line="766"/>
+        <source>Moves</source>
+        <translation type="unfinished">이동 횟수</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="109"/>
+        <location filename="../iqpuzzle.cpp" line="119"/>
+        <location filename="../iqpuzzle.cpp" line="260"/>
+        <location filename="../iqpuzzle.cpp" line="271"/>
+        <source>File not found</source>
+        <translation type="unfinished">파일을 찾을 수 없음</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="110"/>
+        <location filename="../iqpuzzle.cpp" line="120"/>
+        <location filename="../iqpuzzle.cpp" line="261"/>
+        <location filename="../iqpuzzle.cpp" line="272"/>
+        <source>The chosen file does not exist.</source>
+        <translation type="unfinished">선택한 파일이 존재하지 않습니다.</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="136"/>
+        <source>Games share path does not exist!</source>
+        <translation type="unfinished">게임의 공유 경로가 존재하지 않습니다!</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="301"/>
+        <source>Unknown</source>
+        <translation type="unfinished">알수없음</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="306"/>
+        <source>Solutions</source>
+        <translation type="unfinished">해결</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="316"/>
+        <source>Load board</source>
+        <translation type="unfinished">보드 불러오기</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="318"/>
+        <source>Board files</source>
+        <translation type="unfinished">보드 파일</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="394"/>
+        <source>No boards available!</source>
+        <translation type="unfinished">사용가능한 보드 없음!</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="476"/>
+        <location filename="../iqpuzzle.cpp" line="503"/>
+        <source>Save games</source>
+        <translation type="unfinished">게임 저장</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="491"/>
+        <source>Invalid saved puzzle.</source>
+        <translation type="unfinished">저장 가능한 퍼즐이 없습니다.</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="579"/>
+        <source>Puzzle solved!</source>
+        <translation type="unfinished">퍼즐을 풀었습니다!</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="656"/>
+        <source>Statistics</source>
+        <translation type="unfinished">통계</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="664"/>
+        <location filename="../iqpuzzle.cpp" line="690"/>
+        <source>Total</source>
+        <translation type="unfinished">총합</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="666"/>
+        <source>Unsolved</source>
+        <translation type="unfinished">미해결</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="669"/>
+        <source>Easy</source>
+        <translation type="unfinished">쉬움</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="676"/>
+        <source>Medium</source>
+        <translation type="unfinished">중간</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="683"/>
+        <source>Hard</source>
+        <translation type="unfinished">어려움</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="691"/>
+        <source>including unknown difficulty</source>
+        <translation type="unfinished">알수 없는 난이도 포함</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="719"/>
+        <source>About</source>
+        <translation type="unfinished">상세 정보</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="735"/>
+        <source>License</source>
+        <translation type="unfinished">라이선스</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="738"/>
+        <source>This application uses icons from &lt;a href=&quot;http://tango.freedesktop.org&quot;&gt;Tango project&lt;/a&gt;.</source>
+        <translation type="unfinished">이 어플리케이션은 다음 사이트의 아이콘을 사용합니다. &lt;a href=&quot;http://tango.freedesktop.org&quot;&gt;progetto Tango&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="741"/>
+        <source>Translations</source>
+        <translation type="unfinished">번역</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../settings.cpp" line="57"/>
+        <location filename="../settings.cpp" line="201"/>
+        <source>Left</source>
+        <translation type="unfinished">왼쪽</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="57"/>
+        <location filename="../settings.cpp" line="201"/>
+        <source>Middle</source>
+        <translation type="unfinished">중간</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="57"/>
+        <location filename="../settings.cpp" line="201"/>
+        <source>Right</source>
+        <translation type="unfinished">오른쪽</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="60"/>
+        <location filename="../settings.cpp" line="205"/>
+        <source>First X</source>
+        <translation type="unfinished">첫번째 X</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="60"/>
+        <location filename="../settings.cpp" line="205"/>
+        <source>Second X</source>
+        <translation type="unfinished">두번째 X</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="61"/>
+        <location filename="../settings.cpp" line="206"/>
+        <source>Vertical wheel</source>
+        <translation type="unfinished">수직 핸들</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="61"/>
+        <location filename="../settings.cpp" line="206"/>
+        <source>Horizontal wheel</source>
+        <translation type="unfinished">수평 핸들</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="103"/>
+        <source>Please change your settings. Same mouse button is used for several actions.</source>
+        <translation type="unfinished">설정을 변경해 주세요. 여러 작업에 동일한 마우스 버튼이 사용됩니다.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settings.ui" line="20"/>
+        <source>Preferences</source>
+        <translation type="unfinished">참조</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="38"/>
+        <source>Mouse controls</source>
+        <translation type="unfinished">마우스 컨트롤</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="45"/>
+        <source>Move block</source>
+        <translation type="unfinished">블럭 이동</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="55"/>
+        <source>Rotate block</source>
+        <translation type="unfinished">블록 회전</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="65"/>
+        <source>Flip block</source>
+        <translation type="unfinished">블록 뒤집기</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="82"/>
+        <source>GUI language</source>
+        <translation type="unfinished">GUI 언어</translation>
+    </message>
+</context>
+</TS>
