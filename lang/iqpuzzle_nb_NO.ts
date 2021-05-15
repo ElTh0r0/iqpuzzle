b837d89314d40ae9f23d34314c63cc48b7f5ed9a
Allan Nordhøy <epost@anotheragency.no>
2021-05-15 04:46:18 +0200

    Added translation using Weblate (Norwegian Bokmål)

---
+++ lang/iqpuzzle_nb_NO.ts
@@ -0,0 +1,490 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name>Board</name>
+    <message>
+        <location filename="../board.cpp" line="68"/>
+        <location filename="../board.cpp" line="90"/>
+        <location filename="../board.cpp" line="217"/>
+        <location filename="../board.cpp" line="239"/>
+        <location filename="../board.cpp" line="265"/>
+        <location filename="../board.cpp" line="302"/>
+        <location filename="../board.cpp" line="310"/>
+        <location filename="../board.cpp" line="416"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="69"/>
+        <source>Board grid size not valid.
+Reduced grid to default.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="91"/>
+        <source>Board polygon not valid.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="218"/>
+        <location filename="../board.cpp" line="266"/>
+        <source>Polygon not valid:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="240"/>
+        <source>Could not find valid blocks.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="303"/>
+        <source>No color defined - using fallback:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="311"/>
+        <source>Invalid color defined - using fallback:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="417"/>
+        <source>Invalid start position - using fallback:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="187"/>
+        <source>Hint</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../board.cpp" line="188"/>
+        <source>Not all pieces are needed for a solution!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BoardDialog</name>
+    <message>
+        <location filename="../boarddialog.cpp" line="44"/>
+        <location filename="../boarddialog.cpp" line="76"/>
+        <source>Solutions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../boarddialog.cpp" line="45"/>
+        <source>Preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../boarddialog.cpp" line="74"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../boarddialog.cpp" line="50"/>
+        <location filename="../boarddialog.cpp" line="83"/>
+        <source>No preview available</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Highscore</name>
+    <message>
+        <location filename="../highscore.cpp" line="60"/>
+        <location filename="../highscore.cpp" line="160"/>
+        <source>Highscore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../highscore.cpp" line="68"/>
+        <source>Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../highscore.cpp" line="70"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../highscore.cpp" line="72"/>
+        <source>Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../highscore.cpp" line="74"/>
+        <source>Moves</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../highscore.cpp" line="161"/>
+        <source>Please insert your name for a new highscore:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>IQPuzzle</name>
+    <message>
+        <location filename="../iqpuzzle.ui" line="41"/>
+        <source>&amp;Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="89"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="53"/>
+        <source>&amp;All available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="63"/>
+        <source>All &amp;unsolved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="96"/>
+        <source>Setti&amp;ngs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="115"/>
+        <source>Choose &amp;new game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="125"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="135"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="145"/>
+        <source>Zoom &amp;In</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="155"/>
+        <source>Zoom &amp;Out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="168"/>
+        <source>&amp;Restart game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="181"/>
+        <source>&amp;Save game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="184"/>
+        <location filename="../iqpuzzle.cpp" line="514"/>
+        <source>Save game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="194"/>
+        <source>&amp;Load game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="197"/>
+        <location filename="../iqpuzzle.cpp" line="478"/>
+        <source>Load game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="213"/>
+        <source>Pa&amp;use</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="218"/>
+        <source>&amp;Highscore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="228"/>
+        <source>&amp;Configure iQPuzzle...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="233"/>
+        <source>Report &amp;bug</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="238"/>
+        <location filename="../iqpuzzle.ui" line="263"/>
+        <source>&amp;Easy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="243"/>
+        <location filename="../iqpuzzle.ui" line="268"/>
+        <source>&amp;Medium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="248"/>
+        <location filename="../iqpuzzle.ui" line="273"/>
+        <source>&amp;Hard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="253"/>
+        <location filename="../iqpuzzle.ui" line="258"/>
+        <source>&amp;All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="278"/>
+        <source>Sta&amp;tistics</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="684"/>
+        <source>Easy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="691"/>
+        <source>Medium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="698"/>
+        <source>Hard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.ui" line="45"/>
+        <source>R&amp;andom game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="307"/>
+        <source>Load board</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="111"/>
+        <location filename="../iqpuzzle.cpp" line="121"/>
+        <location filename="../iqpuzzle.cpp" line="252"/>
+        <location filename="../iqpuzzle.cpp" line="262"/>
+        <source>File not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="112"/>
+        <location filename="../iqpuzzle.cpp" line="122"/>
+        <location filename="../iqpuzzle.cpp" line="253"/>
+        <location filename="../iqpuzzle.cpp" line="263"/>
+        <source>The chosen file does not exist.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="295"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="494"/>
+        <source>Invalid saved puzzle.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="84"/>
+        <location filename="../iqpuzzle.cpp" line="770"/>
+        <source>Game paused</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="89"/>
+        <location filename="../iqpuzzle.cpp" line="92"/>
+        <location filename="../iqpuzzle.cpp" line="271"/>
+        <location filename="../iqpuzzle.cpp" line="278"/>
+        <location filename="../iqpuzzle.cpp" line="589"/>
+        <source>Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="93"/>
+        <location filename="../iqpuzzle.cpp" line="268"/>
+        <location filename="../iqpuzzle.cpp" line="275"/>
+        <location filename="../iqpuzzle.cpp" line="344"/>
+        <location filename="../iqpuzzle.cpp" line="588"/>
+        <location filename="../iqpuzzle.cpp" line="774"/>
+        <source>Moves</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="309"/>
+        <source>Board files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="299"/>
+        <source>Solutions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="137"/>
+        <source>Games share path does not exist!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="393"/>
+        <source>No boards available!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="480"/>
+        <location filename="../iqpuzzle.cpp" line="516"/>
+        <source>Save games</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="587"/>
+        <source>Puzzle solved!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="671"/>
+        <source>Statistics</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="679"/>
+        <location filename="../iqpuzzle.cpp" line="705"/>
+        <source>Total</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="681"/>
+        <source>Unsolved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="706"/>
+        <source>including unknown difficulty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="726"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="742"/>
+        <source>License</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="745"/>
+        <source>This application uses icons from &lt;a href=&quot;http://tango.freedesktop.org&quot;&gt;Tango project&lt;/a&gt;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../iqpuzzle.cpp" line="748"/>
+        <source>Translations</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../settings.cpp" line="57"/>
+        <location filename="../settings.cpp" line="207"/>
+        <source>Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="57"/>
+        <location filename="../settings.cpp" line="207"/>
+        <source>Middle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="57"/>
+        <location filename="../settings.cpp" line="207"/>
+        <source>Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="60"/>
+        <location filename="../settings.cpp" line="211"/>
+        <source>First X</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="60"/>
+        <location filename="../settings.cpp" line="211"/>
+        <source>Second X</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="61"/>
+        <location filename="../settings.cpp" line="212"/>
+        <source>Horizontal wheel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="61"/>
+        <location filename="../settings.cpp" line="212"/>
+        <source>Vertical wheel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="108"/>
+        <source>Please change your settings. Same mouse button is used for several actions.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settings.ui" line="20"/>
+        <source>Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="42"/>
+        <source>Mouse controls</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="49"/>
+        <source>Move block</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="59"/>
+        <source>Rotate block</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="69"/>
+        <source>Flip block</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="86"/>
+        <source>GUI language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="106"/>
+        <source>Use system background color</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
