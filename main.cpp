e69067f0035b80e04591944e919611cbfc259486
ElTh0r0 <elthoro@gmx.de>
2024-05-01 16:03:27 +0200

    Ci

--- main.cpp
+++ main.cpp
@@ -56,7 +56,7 @@ auto main(int argc, char *argv[]) -> int {
   app.setApplicationName(QStringLiteral(APP_NAME));
   app.setApplicationVersion(QStringLiteral(APP_VERSION));
   app.setApplicationDisplayName(QStringLiteral(APP_NAME));
-#if !defined(Q_OS_WIN) && !defined(Q_OS_MAC)
+#if !defined(Q_OS_WIN) && !defined(Q_OS_MACOS)
   app.setWindowIcon(QIcon::fromTheme(QStringLiteral("iqpuzzle"),
                                      QIcon(QStringLiteral(":/iqpuzzle.png"))));
 #if QT_VERSION >= QT_VERSION_CHECK(5, 7, 0)
@@ -86,7 +86,7 @@ auto main(int argc, char *argv[]) -> int {
     sSharePath = app.applicationDirPath() + "/../share/" +
                  app.applicationName().toLower();
   }
-#if defined(Q_OS_OSX)
+#if defined(Q_OS_MACOS)
   sSharePath = app.applicationDirPath() + "/../Resources/";
 #endif
 
